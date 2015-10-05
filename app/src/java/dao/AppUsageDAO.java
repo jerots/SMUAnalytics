cf5cfbf0e23340a433e8d00fbd89c49d24de0320
zhihui <zhihui@10.124.128.29>
2015-10-05 17:24:07 +0800

    resolve conflict

--- app/src/java/dao/AppUsageDAO.java
+++ app/src/java/dao/AppUsageDAO.java
@@ -20,28 +20,6 @@ public class AppUsageDAO {
 	public AppUsageDAO() {
 	}
 
-<<<<<<< HEAD
-    public void insert(AppDAO aDao, UserDAO uDao, CSVReader reader) throws IOException, SQLException {
-        try{
-            Connection conn = ConnectionManager.getConnection();
-            conn.setAutoCommit(false);
-            String sql = "insert into appusage (timestamp, macaddress, appid) values(STR_TO_DATE(?,'%Y-%m-%d %H:%i:%s'),?,?) ON DUPLICATE KEY UPDATE appid "
-                    + "= VALUES(appid);";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            int counter = 0;
-            String[] arr = null;
-            while ((arr = reader.readNext()) != null) {
-                //retrieving per row
-                boolean err = false;
-                //check timestamp
-                java.util.Date format = Utility.parseDate(arr[0]);
-                
-                if (format == null) {
-                    err = true;
-                    unsuccessful.add("invalid timestamp");
-                }
-                String date = Utility.formatDate(format);
-=======
 	public void insert(AppDAO aDao, UserDAO uDao, CSVReader reader) throws IOException, SQLException {
 		try {
 			Connection conn = ConnectionManager.getConnection();
@@ -62,7 +40,6 @@ public class AppUsageDAO {
 					unsuccessful.add("invalid timestamp");
 				}
 				String date = Utility.formatDate(format);
->>>>>>> 83d6505dcf16baa5397eebea5208ac96672b573d
 
 				//check macAdd
 				String macAdd = Utility.parseString(arr[1]);
@@ -92,34 +69,6 @@ public class AppUsageDAO {
 					err = true;
 				}
 
-<<<<<<< HEAD
-
-                if (!err) {
-                    counter++;
-                    //add to list
-                    stmt.setString(1, date);
-                    stmt.setString(2, macAdd);
-                    stmt.setInt(3, appId);
-                    stmt.addBatch();
-                    //insert into tables
-                }
-            }
-            //closing
-            if (stmt != null) {
-                int[] errors = stmt.executeBatch();
-                System.out.println(errors.length);
-                for(int i = 0; i < counter - errors.length; i++){
-                    unsuccessful.add("duplicate row");
-                    System.out.println(i);
-                }
-                conn.commit();
-            }
-            reader.close();
-            ConnectionManager.close(conn,stmt);
-        }catch(NullPointerException e){
-        }
-    }
-=======
 				if (!err) {
 					//add to list
 					stmt.setString(1, date);
@@ -169,7 +118,6 @@ public class AppUsageDAO {
 
 		return null;
 	}
->>>>>>> 83d6505dcf16baa5397eebea5208ac96672b573d
 
 
 }
