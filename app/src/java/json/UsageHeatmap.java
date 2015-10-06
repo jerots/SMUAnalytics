b7fd7b5f8eeda06f71330e2553ca4ffbf504a786
zhihui <zhihui@10.124.128.29>
2015-10-06 16:37:05 +0800

    updated json validation for date and time

--- app/src/java/json/UsageHeatmap.java
+++ app/src/java/json/UsageHeatmap.java
@@ -9,7 +9,11 @@ import is203.JWTException;
 import is203.JWTUtility;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,104 +28,130 @@ import net.minidev.json.JSONObject;
 @WebServlet(name = "UsageHeatmap", urlPatterns = {"/usage-heatmap"})
 public class UsageHeatmap extends HttpServlet {
 
-	/**
-	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-	 * methods.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		response.setContentType("application/json");
-		try (PrintWriter out = response.getWriter()) {
-			JSONObject result = new JSONObject();
-			ArrayList<String> errors = new ArrayList<String>();
-			
-			
-			String token = request.getParameter("token");
-			String date = request.getParameter("date");
-			String time = request.getParameter("time");
-			String floor = request.getParameter("floor");
-			
-			try {
-				String username = JWTUtility.verify(token, "nabjemzhdarrensw");
-				if (username == null){
-					//failed
-				}
-			} catch (JWTException e){
-				//failed
-				e.printStackTrace();
-			}
-			
-			
-			//FLOOR VALIDATION
-			if (floor == null){
-				errors.add("missing floor");
-			} else {
-				try {
-					int floorInt = Integer.parseInt(floor);
-					if (floorInt < 0 || floorInt > 5){
-						errors.add("invalid floor");
-					}
-				} catch (NumberFormatException e){
-					errors.add("invalid floor");
-				}
-				
-			}
-			
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        try (PrintWriter out = response.getWriter()) {
+            JSONObject result = new JSONObject();
+            ArrayList<String> errors = new ArrayList<String>();
+
+            String token = request.getParameter("token");
+            String date = request.getParameter("date");
+            String time = request.getParameter("time");
+            String floor = request.getParameter("floor");
+
+            try {
+                String username = JWTUtility.verify(token, "nabjemzhdarrensw");
+                if (username == null) {
+                    //failed
+                }
+            } catch (JWTException e) {
+                //failed
+                e.printStackTrace();
+            }
+
+            //FLOOR VALIDATION
+            if (floor == null) {
+                errors.add("missing floor");
+            } else if (floor.length() == 0) {
+                errors.add("blank floor");
+            } else {
+                try {
+                    int floorInt = Integer.parseInt(floor);
+                    if (floorInt < 0 || floorInt > 5) {
+                        errors.add("invalid floor");
+                    }
+                } catch (NumberFormatException e) {
+                    errors.add("invalid floor");
+                }
+
+            }
+
 			//DATE VALIDATION
-			
-			
-			
+            if (date == null) {
+                errors.add("missing date");
+            } else if (date.length() == 0) {
+                errors.add("blank date");
+            } else {
+                if(date.length() != 10) {
+                    errors.add("Invalid date");    
+                } else {
+                    SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+                    Date dateFormatted = sdf.parse(date, new ParsePosition(0));
+                    if(dateFormatted == null) {
+                        errors.add("Invalid date");   
+                    }
+                }
+            }
+
 			//TIME VALIDATION
-			
-			
-			
-			
-			
-		}
-	}
+            if(time == null) {
+                errors.add("missing time");
+            } else if(time.length() == 0) {
+                errors.add("blank time");
+            } else {
+                if(time.length() != 8) {
+                    errors.add("Invalid time");
+                } else {
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                    Date dateFormatted = sdf.parse(time, new ParsePosition(0));
+                    if(dateFormatted == null) {
+                        errors.add("Invalid time");
+                    }
+                }
+            }
+            
+            RequestDispatcher rd = request.getRequestDispatcher("HeatmapAction.java");
+            rd.forward(request, response);
+        }
+    }
 
-	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		processRequest(request, response);
-	}
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		processRequest(request, response);
-	}
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
-	/**
-	 * Returns a short description of the servlet.
-	 *
-	 * @return a String containing servlet description
-	 */
-	@Override
-	public String getServletInfo() {
-		return "Short description";
-	}// </editor-fold>
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 
 }
