726facc376a559b80cdf8077a433fc7e5a535d09
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2015-10-08 14:41:42 +0800

    Basic app report JSON input validation completed. BasicAppController input and output implemented. Next step: controller logic

---
+++ app/src/java/controller/BasicAppController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ *
+ * @author jeremyongts92
+ */
+public class BasicAppController {
+	
+	
+	public HashMap<String, String[]> generateReport (Date startDate, Date endDate){
+		
+		return null;
+	}
+	
+}
--- app/src/java/json/BasicUsetimeReport.java
+++ app/src/java/json/BasicUsetimeReport.java
@@ -5,6 +5,7 @@
  */
 package json;
 
+import controller.BasicAppController;
 import controller.HeatmapController;
 import entity.LocationUsage;
 import is203.JWTException;
@@ -17,7 +18,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,8 +30,8 @@ import net.minidev.json.JSONObject;
  *
  * @author jeremyongts92
  */
-@WebServlet(name = "UsageHeatmap", urlPatterns = {"/json/usage-heatmap"})
-public class UsageHeatmap extends HttpServlet {
+@WebServlet(name = "BasicUsetimeReport", urlPatterns = {"/json/basic-usetime-report"})
+public class BasicUsetimeReport extends HttpServlet {
 
 	/**
 	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -50,149 +50,117 @@ public class UsageHeatmap extends HttpServlet {
 			JSONArray errors = new JSONArray();
 
 			String token = request.getParameter("token");
-			String date = request.getParameter("date");
-			String time = request.getParameter("time");
-			String floor = request.getParameter("floor");
-			
-			
+			String startdate = request.getParameter("startdate");
+			String enddate = request.getParameter("enddate");
+
 			//TOKEN VALIDATION
-			
-			if (token == null){
+			if (token == null) {
 				errors.add("missing token");
-			} else if (token.length() == 0){
+			} else if (token.length() == 0) {
 				errors.add("blank token");
 			} else {
 				try {
-				String username = JWTUtility.verify(token, "nabjemzhdarrensw");
-				if (username == null) {
+					String username = JWTUtility.verify(token, "nabjemzhdarrensw");
+					if (username == null) {
+						//failed
+						errors.add("invalid token");
+					}
+
+				} catch (JWTException e) {
 					//failed
 					errors.add("invalid token");
 				}
-				
-			
-			} catch (JWTException e) {
-				//failed
-				errors.add("invalid token");
-			}
-			
-				
-			}
-			
-			
-			
-		
-			//FLOOR VALIDATION
-			if (floor == null) {
-				errors.add("missing floor");
-			} else if (floor.length() == 0) {
-				errors.add("blank floor");
-			} else {
-				try {
-					int floorInt = Integer.parseInt(floor);
-					if (floorInt < 0 || floorInt > 5) {
-						errors.add("invalid floor");
-					} else {
-						//parse floor to be controller-readable format e.g. B1, L1, L2...
-						if (floorInt == 0){
-							floor = "B1";
-						} else {
-							floor = "L" + floor;
-						}
-					}
-				} catch (NumberFormatException e) {
-					errors.add("invalid floor");
-				}
 
 			}
 
-			//DATE VALIDATION
-			if (date == null) {
-				errors.add("missing date");
-			} else if (date.length() == 0) {
-				errors.add("blank date");
+			
+
+			//START DATE VALIDATION
+			if (startdate == null) {
+				errors.add("missing startdate");
+			} else if (startdate.length() == 0) {
+				errors.add("blank startdate");
 			} else {
-				if (date.length() != 10) {
-					errors.add("invalid date");
+				if (startdate.length() != 10) {
+					errors.add("invalid startdate");
 				} else {
 					SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
-					Date dateFormatted = sdf.parse(date, new ParsePosition(0));
+					Date dateFormatted = sdf.parse(startdate, new ParsePosition(0));
 					if (dateFormatted == null) {
-						errors.add("invalid date");
+						errors.add("invalid startdate");
+					}
+				}
+			}
+			
+			//END DATE VALIDATION
+			if (enddate == null) {
+				errors.add("missing enddate");
+			} else if (enddate.length() == 0) {
+				errors.add("blank enddate");
+			} else {
+				if (enddate.length() != 10) {
+					errors.add("invalid enddate");
+				} else {
+					SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+					Date dateFormatted = sdf.parse(enddate, new ParsePosition(0));
+					if (dateFormatted == null) {
+						errors.add("invalid enddate");
 					}
 				}
 			}
 
-			//TIME VALIDATION
-			if (time == null) {
-				errors.add("missing time");
-			} else if (time.length() == 0) {
-				errors.add("blank time");
-			} else {
-				if (time.length() != 8) {
-					errors.add("invalid time");
-				} else {
-					SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-					Date dateFormatted = sdf.parse(time, new ParsePosition(0));
-					if (dateFormatted == null) {
-						errors.add("invalid time");
-					}
-				}
-			}
 			
-			
+
 			//PRINT ERROR AND EXIT IF ERRORS EXIST
-			if (errors.size() > 0){
+			if (errors.size() > 0) {
 				output.put("status", "error");
 				output.put("errors", errors);
 				out.println(output.toJSONString());
 				return;
 			}
-			
-			
-			
+
 			//PASSES ALL VALIDATION, proceed to report generation
-
-			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			Date datetime = dateFormat.parse(date + " " + time, new ParsePosition(0));
-
-			HeatmapController ctrl = new HeatmapController();
-			HashMap<String, ArrayList<LocationUsage>> luMap = ctrl.generateHeatmap(datetime, floor);
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Date startDate = dateFormat.parse(startdate, new ParsePosition(0));
+			Date endDate = dateFormat.parse(enddate, new ParsePosition(0));
+			
+			BasicAppController ctrl = new BasicAppController();
+			HashMap<String, String[]> resultMap = ctrl.generateReport(startDate, endDate);
 			
 			
-			
-			output.put("status", "success");
-			JSONArray heatmap = new JSONArray();
-			Iterator<String> iter = luMap.keySet().iterator();
+//			output.put("status", "success");
+//			JSONArray heatmap = new JSONArray();
+//			Iterator<String> iter = luMap.keySet().iterator();
+//
+//			while (iter.hasNext()) {
+//				JSONObject currLoc = new JSONObject();
+//
+//				String loc = iter.next();
+//				currLoc.put("semanatic-place", loc);
+//				int numUsers = luMap.get(loc).size();
+//				currLoc.put("num-people-using-phone", numUsers);
+//
+//				int density = 0;
+//				if (numUsers == 0) {
+//
+//				} else if (numUsers < 4) {
+//					density = 1;
+//				} else if (numUsers < 8) {
+//					density = 2;
+//				} else if (numUsers < 14) {
+//					density = 3;
+//				} else if (numUsers < 21) {
+//					density = 4;
+//				} else {
+//					density = 5;
+//				}
+//				currLoc.put("crowd-density", density);
+//				heatmap.add(currLoc);
+//			}
+//
+//			output.put("heatmap", heatmap);
+//			out.println(output.toJSONString());
 
-			while (iter.hasNext()) {
-				JSONObject currLoc = new JSONObject();
-
-				String loc = iter.next();
-				currLoc.put("semanatic-place", loc);
-				int numUsers = luMap.get(loc).size();
-				currLoc.put("num-people-using-phone", numUsers);
-
-				int density = 0;
-				if (numUsers == 0) {
-
-				} else if (numUsers < 4) {
-					density = 1;
-				} else if (numUsers < 8) {
-					density = 2;
-				} else if (numUsers < 14) {
-					density = 3;
-				} else if (numUsers < 21) {
-					density = 4;
-				} else {
-					density = 5;
-				}
-				currLoc.put("crowd-density", density);
-				heatmap.add(currLoc);
-			}
-
-			output.put("heatmap", heatmap);
-			out.println(output.toJSONString());
-			
 		}
 	}
 
