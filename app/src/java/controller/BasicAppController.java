3c801b2ac1db1d6a66ceaabbac4380bb40d5805c
darren.tay.2014 <darren.tay.2014@smu.edu.sg>
2015-11-11 17:11:12 +0800

    Merge origin/master
    Conflicts:
    	app/src/java/controller/BasicAppController.java
    	app/src/java/dao/AppUsageDAO.java
    	app/src/java/dao/LocationUsageDAO.java
    	app/src/java/dao/Utility.java
    	app/src/java/servlet/admin/BootstrapAction.java
    	app/web/bootstrap.jsp

--- app/src/java/controller/BasicAppController.java
+++ app/src/java/controller/BasicAppController.java
@@ -163,9 +163,9 @@ public class BasicAppController {
 
         Breakdown result = new Breakdown();
 
-        ArrayList<String> schools = Utility.getSchools();
+        ArrayList<String> schools = Utility.retrieveSchools();
         ArrayList<String> years = Utility.getYears();
-        ArrayList<String> genders = userDAO.getGenders();
+        ArrayList<String> genders = Utility.getGenders();
         ArrayList<String> ccas = userDAO.getCCAs();
 
         ArrayList<String> demo1List = new ArrayList<String>();
